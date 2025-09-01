b0a43bd30aa3df28834cee9edab5a5de6a0cea9e
dwarftee <dwarftee@protonmail.com>
2025-09-01 16:57:01 +0100

    commit :0

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <cmath>
-
-double sigmoid(double x) {
-  return 1.0 / (1.0 + exp(-x));
-}
+#include "sigmoid.h"
 
 int main() {
-
+  double x = sigmoid(7);
+  std::cout << x;
 }
---
+++ sigmoid.cpp
@@ -0,0 +1,5 @@
+#include <cmath>
+
+double sigmoid(double x) {
+  return 1.0 / (1.0 + exp(-x));
+}
