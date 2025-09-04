596661178c80fc8dad724ed7a8c8e985fe200fbc
dwarftee <dwarftee@protonmail.com>
2025-09-04 11:19:52 +0100

    commit :0

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 #include <cmath>
-#include "sigmoid.h"
+#include "mathslib.h"
 
 int main() {
-  double x = sigmoid(7);
-  std::cout << x;
+  int training_inputs[8] = {0 , 0,
+                            0 , 1,
+                            1 , 0,
+                            1 , 1};
+  int training_outputs[4] = {0,
+                             0,
+                             0,
+                             1};
+  double x1 = 0.0;
+  double x2 = 1.0;
+
+  double w1 = 4.0;
+  double w2 = 4.0;
+
+  double b = 2.0;
+
+  double hidden1 = (x1 * w1) + (x2 * w2) + b;
+ 
+  double output = sigmoid(hidden1);
+  
+  std::cout << output << "\n";
+  std::cout << hidden1 << "\n";
+
 }
